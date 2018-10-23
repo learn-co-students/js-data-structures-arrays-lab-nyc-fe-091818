ccc4d57839a42e5387be7010203fd57c832bb884
Katelyn Junker <kjunker@villanova.edu>
2018-10-23 23:23:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const drivers= ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift()
+}
+
+function appendDriver(name){
+  const driversCopy = drivers.slice()
+  driversCopy.push(name)
+  return driversCopy
+}
+
+function prependDriver(name){
+  const driversCopy = drivers.slice()
+  driversCopy.unshift(name)
+  return driversCopy
+}
+
+function removeLastDriver(){
+  const driversCopy = drivers.slice()
+  driversCopy.pop()
+  return driversCopy
+}
+
+function removeFirstDriver(){
+  const driversCopy = drivers.slice()
+  driversCopy.shift()
+  return driversCopy
+}
